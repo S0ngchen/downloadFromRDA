e8fdc8b9f2e826147bccaa85cd53ca65087db29e
leosssssss <1536141948@qq.com>
2024-04-19 11:53:49 +0800

    update the script

---
+++ rda-download.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+""" 
+Python script to download selected files from rda.ucar.edu.
+After you save the file, don't forget to make it executable
+i.e. - "chmod 755 <name_of_script>"
+"""
+
+url = 'https://data.rda.ucar.edu/ds083.2/grib2/'
+import sys, os
+from urllib.request import build_opener
+from datetime import datetime, timedelta
+import time
+import urllib
+import random
+
+
+start_time = datetime.strptime('20110801_12_00', '%Y%m%d_%H_%M')
+end_time = datetime(2023, 12, 31, 0, 0, 0)
+time_list = (start_time + timedelta(hours=6*i) for i in range(int((end_time - start_time).total_seconds()) // (3600*6 + 1)))
+time_list_formatted = (time.strftime('%Y%m%d_%H_%M') for time in time_list)
+filelist = (f'{url}{time[0:4]}/{time[0:4]}.{time[4:6]}/fnl_{time}.grib2' for time in time_list_formatted)
+
+
+opener = build_opener()
+i = 1
+for file in filelist:
+    if i <= 30:
+        i += 1
+    else:
+        i = 1
+    ofile = os.path.basename(file)
+    sys.stdout.write("downloading " + ofile + " ... ")
+    sys.stdout.flush()
+    infile = opener.open(file)
+    outfile = open(ofile, "wb")
+    outfile.write(infile.read())
+    outfile.close()
+    sys.stdout.write("done\n")
+
---
+++ scrpitCTL.py
@@ -0,0 +1,45 @@
+import subprocess
+import time
+
+
+def run_and_monitor(script_path):
+    try:
+        process = subprocess.Popen(['python', script_path], stdout=subprocess.PIPE, text=True)
+        with open(script_path, 'r') as file:
+            lines = file.readlines()
+        date = lines[16][32:46]
+        file.close()
+        output = 'done'
+        while True:
+            newC = f"start_time = datetime.strptime('{date}', '%Y%m%d_%H_%M')"
+            modify_script_line(script_path, 17, newC)
+            return_code = process.poll()
+            output = process.stdout.readline().strip()
+            if output is not None:
+                date = output[16:30]
+            print(output)
+            if return_code is not None:
+                date = lines[16][32:46]
+                process = subprocess.Popen(['python', script_path], stdout=subprocess.PIPE, text=True)
+            time.sleep(0.1)
+    except subprocess.CalledProcessError as e:
+        print("error:", e)
+
+
+def modify_script_line(script_path, line_number, new_content):
+    try:
+        with open(script_path, 'r') as file:
+            lines = file.readlines()
+        if 0 < line_number <= len(lines):
+            lines[line_number - 1] = new_content + '\n'
+            with open(script_path, 'w') as file:
+                file.writelines(lines)
+        else:
+            print("out of range")
+    except IOError as e:
+        print("operation error:", e)
+
+
+run_and_monitor(r"E:\analized data\rda-download.py")
+
+
