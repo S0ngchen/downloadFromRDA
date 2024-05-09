7761b8d7df96d5e0c3d614f05978dfab6668a10a
leosssssss <105504028+leosssssss@users.noreply.github.com>
2024-05-09 22:38:25 +0800

    Update rda-download.py
    added some comments to introduce scripts

--- rda-download.py
+++ rda-download.py
@@ -14,11 +14,15 @@ import urllib
 import random
 
 
-start_time = datetime.strptime('YYYYMMDD_HH_MM', '%Y%m%d_%H_%M')    # start time, format: YYYYMMDD_HH_MM
+start_time = datetime.strptime('YYYYMMDD_HH_MM', '%Y%m%d_%H_%M')    # start time, format: YYYYMMDD_HH_MM, may be changed in different situations
 end_time = datetime(2023, 12, 31, 0, 0, 0)
-time_list = (start_time + timedelta(hours=6*i) for i in range(int((end_time - start_time).total_seconds()) // (3600*6 + 1)))
+time_list = (start_time + timedelta(hours=6*i) for i in range(int((end_time - start_time).total_seconds()) // (3600*6 + 1)))    # steps in ds083.2 is 6 hours, may be changed in different situations 
 time_list_formatted = (time.strftime('%Y%m%d_%H_%M') for time in time_list)
-filelist = (f'{url}{time[0:4]}/{time[0:4]}.{time[4:6]}/fnl_{time}.grib2' for time in time_list_formatted)
+filelist = (f'{url}{time[0:4]}/{time[0:4]}.{time[4:6]}/fnl_{time}.grib2' for time in time_list_formatted)    # EXAMPLE url, may be changed in different situations
+"""
+The desired download format can be found in the original download script
+For EXAMPLE, the format of url provided here is 'https://data.rda.ucar.edu/ds083.2/grib2/YYYY/MM/fnl_YYYYMMDD_HH_MM.grib2'. As for ds083.3, the example url may be 'https://data.rda.ucar.edu/ds083.3/YYYY/YYYYMM/gdas1.fnl0p25.YYYYMMDDHH.fXX.grib2'.(XX here means XX hours later)
+"""
 
 
 opener = build_opener()
